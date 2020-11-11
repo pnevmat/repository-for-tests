c9feeceb9ec394d91b4195462b58120b66082654
Vadim Kravchenko <pnevmat001@gmail.com>
2020-11-11 14:51:58 +0200

    Upgrades button js code

--- js/button-choose.js
+++ js/button-choose.js
@@ -4,164 +4,214 @@ const appsButtonRef = document.querySelector('button[apps]');
 const designButtonRef = document.querySelector('button[design]');
 const marketingButtonRef = document.querySelector('button[marketing]');
 
-const webSitesCardsRef = document.querySelectorAll('li[web-site]');
-const appsCardsRef = document.querySelectorAll('li[app]');
-const designCardsRef = document.querySelectorAll('li[design]');
-const marketingCardsRef = document.querySelectorAll('li[marketing]');
+const allCardsRef = [...document.querySelectorAll('li[web-site]'),
+    ...document.querySelectorAll('li[app]'),
+    ...document.querySelectorAll('li[design]'),
+    ...document.querySelectorAll('li[marketing]')];
 
-let listLength;
+webSitesButtonRef.addEventListener('click', onWebSitesButtonClick);
+appsButtonRef.addEventListener('click', onAppsButtonClick);
+designButtonRef.addEventListener('click', onDesignButtonClick);
+marketingButtonRef.addEventListener('click', onMarketingButtonClick);
+allButtonRef.addEventListener('click', onAllButtonClick);
 
-webSitesButtonRef.addEventListener('click', () => {
-    listLength = appsCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            appsCardsRef[i].classList.add('is-hidden');
+function onWebSitesButtonClick() {
+    allCardsRef.forEach((element, i) => {
+        if (element.attributes[1].name !== 'web-site') {
+            allCardsRef[i].classList.add('is-hidden');
         } else {
-            break;
+            allCardsRef[i].classList.remove('is-hidden');
         }
-    }
-    listLength = designCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            designCardsRef[i].classList.add('is-hidden');
+    });
+};
+function onAppsButtonClick() {
+    allCardsRef.forEach((element, i) => {
+        if (element.attributes[1].name !== 'app') {
+            allCardsRef[i].classList.add('is-hidden');
         } else {
-            break;
+            allCardsRef[i].classList.remove('is-hidden');
         }
-    }
-    listLength = marketingCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            marketingCardsRef[i].classList.add('is-hidden');
+    });
+};
+function onDesignButtonClick() {
+    allCardsRef.forEach((element, i) => {
+        if (element.attributes[1].name !== 'design') {
+            allCardsRef[i].classList.add('is-hidden');
         } else {
-            break;
+            allCardsRef[i].classList.remove('is-hidden');
         }
-    }
-    listLength = webSitesCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        webSitesCardsRef[i].classList.remove('is-hidden');
-    }
-});
+    });
+};
+function onMarketingButtonClick() {
+    allCardsRef.forEach((element, i) => {
+        if (element.attributes[1].name !== 'marketing') {
+            allCardsRef[i].classList.add('is-hidden');
+        } else {
+            allCardsRef[i].classList.remove('is-hidden');
+        }
+    });
+};
+function onAllButtonClick() {
+    allCardsRef.forEach((element, i) => {
+        allCardsRef[i].classList.remove('is-hidden');
+    });
+};
 
-appsButtonRef.addEventListener('click', () => {
-    listLength = webSitesCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        webSitesCardsRef[i].classList.add('is-hidden');
-    }
-    listLength = designCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            designCardsRef[i].classList.add('is-hidden');
-        } else {
-            break;
-        }
-    }
-    listLength = marketingCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            marketingCardsRef[i].classList.add('is-hidden');
-        } else {
-            break;
-        }
-    }
-    listLength = appsCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            appsCardsRef[i].classList.remove('is-hidden');
-        } else {
-            break;
-        }
-    }
-});
+// let listLength;
 
-designButtonRef.addEventListener('click', () => {
-    listLength = webSitesCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        webSitesCardsRef[i].classList.add('is-hidden');
-    }
-    listLength = appsCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            appsCardsRef[i].classList.add('is-hidden');
-        } else {
-            break;
-        }
-    }
-    listLength = marketingCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            marketingCardsRef[i].classList.add('is-hidden');
-        } else {
-            break;
-        }
-    }
-    listLength = designCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            designCardsRef[i].classList.remove('is-hidden');
-        } else {
-            break;
-        }
-    }
-});
+// webSitesButtonRef.addEventListener('click', () => {
+//     listLength = appsCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         if (i < listLength) {
+//             appsCardsRef[i].classList.add('is-hidden');
+//         } else {
+//             break;
+//         }
+//     }
+//     listLength = designCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         if (i < listLength) {
+//             designCardsRef[i].classList.add('is-hidden');
+//         } else {
+//             break;
+//         }
+//     }
+//     listLength = marketingCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         if (i < listLength) {
+//             marketingCardsRef[i].classList.add('is-hidden');
+//         } else {
+//             break;
+//         }
+//     }
+//     listLength = webSitesCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         webSitesCardsRef[i].classList.remove('is-hidden');
+//     }
+// });
 
-marketingButtonRef.addEventListener('click', () => {
-    listLength = webSitesCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        webSitesCardsRef[i].classList.add('is-hidden');
-    }
-    listLength = appsCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            appsCardsRef[i].classList.add('is-hidden');
-        } else {
-            break;
-        }
-    }
-    listLength = designCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            designCardsRef[i].classList.add('is-hidden');
-        } else {
-            break;
-        }
-    }
-    listLength = marketingCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            marketingCardsRef[i].classList.remove('is-hidden');
-        } else {
-            break;
-        }
-    }
-});
+// appsButtonRef.addEventListener('click', () => {
+//     listLength = webSitesCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         webSitesCardsRef[i].classList.add('is-hidden');
+//     }
+//     listLength = designCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         if (i < listLength) {
+//             designCardsRef[i].classList.add('is-hidden');
+//         } else {
+//             break;
+//         }
+//     }
+//     listLength = marketingCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         if (i < listLength) {
+//             marketingCardsRef[i].classList.add('is-hidden');
+//         } else {
+//             break;
+//         }
+//     }
+//     listLength = appsCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         if (i < listLength) {
+//             appsCardsRef[i].classList.remove('is-hidden');
+//         } else {
+//             break;
+//         }
+//     }
+// });
 
-allButtonRef.addEventListener('click', () => {
-    listLength = webSitesCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        webSitesCardsRef[i].classList.remove('is-hidden');
-    }
-    listLength = appsCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            appsCardsRef[i].classList.remove('is-hidden');
-        } else {
-            break;
-        }
-    }
-    listLength = designCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            designCardsRef[i].classList.remove('is-hidden');
-        } else {
-            break;
-        }
-    }
-    listLength = marketingCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            marketingCardsRef[i].classList.remove('is-hidden');
-        } else {
-            break;
-        }
-    }
-});
+// designButtonRef.addEventListener('click', () => {
+//     listLength = webSitesCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         webSitesCardsRef[i].classList.add('is-hidden');
+//     }
+//     listLength = appsCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         if (i < listLength) {
+//             appsCardsRef[i].classList.add('is-hidden');
+//         } else {
+//             break;
+//         }
+//     }
+//     listLength = marketingCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         if (i < listLength) {
+//             marketingCardsRef[i].classList.add('is-hidden');
+//         } else {
+//             break;
+//         }
+//     }
+//     listLength = designCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         if (i < listLength) {
+//             designCardsRef[i].classList.remove('is-hidden');
+//         } else {
+//             break;
+//         }
+//     }
+// });
+
+// marketingButtonRef.addEventListener('click', () => {
+//     listLength = webSitesCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         webSitesCardsRef[i].classList.add('is-hidden');
+//     }
+//     listLength = appsCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         if (i < listLength) {
+//             appsCardsRef[i].classList.add('is-hidden');
+//         } else {
+//             break;
+//         }
+//     }
+//     listLength = designCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         if (i < listLength) {
+//             designCardsRef[i].classList.add('is-hidden');
+//         } else {
+//             break;
+//         }
+//     }
+//     listLength = marketingCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         if (i < listLength) {
+//             marketingCardsRef[i].classList.remove('is-hidden');
+//         } else {
+//             break;
+//         }
+//     }
+// });
+
+// allButtonRef.addEventListener('click', () => {
+//     listLength = webSitesCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         webSitesCardsRef[i].classList.remove('is-hidden');
+//     }
+//     listLength = appsCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         if (i < listLength) {
+//             appsCardsRef[i].classList.remove('is-hidden');
+//         } else {
+//             break;
+//         }
+//     }
+//     listLength = designCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         if (i < listLength) {
+//             designCardsRef[i].classList.remove('is-hidden');
+//         } else {
+//             break;
+//         }
+//     }
+//     listLength = marketingCardsRef.length;
+//     for (let i = 0; i < listLength; i += 1) {
+//         if (i < listLength) {
+//             marketingCardsRef[i].classList.remove('is-hidden');
+//         } else {
+//             break;
+//         }
+//     }
+// });
+
+// debugger;
